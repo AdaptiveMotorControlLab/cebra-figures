3afa4950ba0287d95646310acac5457e65741827
Jin Lee <45169672+jinhl9@users.noreply.github.com>
2022-08-23 17:21:27 +0200

    Add additional figures for monkey mse & extended synthetic exps (#12)
    * Add additional figures for monkey mse & extended synthetic exps
    
    * Add monkey mse exp visualization

--- src/FigureS9.py
+++ src/FigureS9.py
@@ -5,7 +5,7 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.13.7
+#       jupytext_version: 1.14.1
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
@@ -27,7 +27,7 @@ from matplotlib import cm, colors
 
 # -
 
-data = pd.read_hdf("../data/FigureS9.h5")
+data = pd.read_hdf('../data/FigureS9.h5')
 
 # ## Figure S9a
 
@@ -46,11 +46,11 @@ ax.plot(ridge_acc_time, label="Ridge classifier", color="k", alpha=0.2, lw=5)
 ax.spines["right"].set_visible(False)
 ax.spines["top"].set_visible(False)
 plt.legend(frameon=False, fontsize=12)
-plt.ylabel("ACC (%)", fontsize=20)
-plt.yticks(np.linspace(20, 100, 5), fontsize=15)
-plt.xlabel("Time from movement onset (ms)", fontsize=20)
+plt.ylabel("ACC (%)", fontsize=25)
+plt.yticks(np.linspace(20, 100, 5), fontsize=25)
+plt.xlabel("Time from movement onset (ms)", fontsize=25)
 plt.xticks(
-    np.linspace(0, 600, 7, dtype=int), np.linspace(-100, 500, 7, dtype=int), fontsize=15
+    np.linspace(0, 600, 7, dtype=int), np.linspace(-100, 500, 7, dtype=int), fontsize=25
 )
 plt.show()
 # -
@@ -58,8 +58,13 @@ plt.show()
 # ## Figure S9b
 
 # +
-fig = plt.figure(figsize=(18, 6))
-plt.subplots_adjust(wspace=0.8)
+
+
+fig = plt.figure(figsize=(6,6))
+ax1 = fig.add_subplot(111)
+ax1.set_title("InfoNCE, all trials", fontsize=30)
+ax1.spines["top"].set_visible(False)
+ax1.spines["right"].set_visible(False)
 
 all_loss_data = data["all_trial_loss"]
 all_ap = all_loss_data["ap"]
@@ -73,11 +78,6 @@ all_appostarget = all_loss_data["ap_pos_target"]
 all_ap_sh = all_loss_data["ap_shuffle"]
 all_pos_sh = all_loss_data["pos_shuffle"]
 all_target_sh = all_loss_data["target_shuffle"]
-
-ax1 = fig.add_subplot(121)
-ax1.set_title("InfoNCE, all trials")
-ax1.spines["top"].set_visible(False)
-ax1.spines["right"].set_visible(False)
 
 w = 9
 p = 7
@@ -116,8 +116,8 @@ ax1.plot(scipy.signal.savgol_filter(all_appostarget, w, p), label="ap+pos+target
 
 plt.xlim([0, 10000])
 plt.xlabel("Steps", fontsize=30)
-plt.xticks(np.linspace(0, 10000, 5), fontsize=25)
-plt.yticks(np.linspace(6.2, 5.6, 4), fontsize=25)
+plt.xticks(np.linspace(0, 10000, 5), fontsize=20)
+plt.yticks(np.linspace(6.2, 5.6, 4), fontsize=20)
 plt.ylabel("Loss", fontsize=30)
 
 
@@ -147,24 +147,32 @@ ax1.legend(
         "position shuffled",
         "a/p shuffled",
     ],
-    loc=(1.0, 0),
+    loc=(1.1, 0),
     frameon=False,
     fontsize=15,
 )
 
 
-ax2 = fig.add_subplot(122)
-ax2.set_title("InfoNCE, active trials")
+
+fig = plt.figure(figsize=(13, 6))
+plt.subplots_adjust(wspace=0.3)
+
+ax2 = fig.add_subplot(121)
+ax2.set_title("InfoNCE, active trials", fontsize=30)
 ax2.spines["top"].set_visible(False)
 ax2.spines["right"].set_visible(False)
 
-active_loss_data = data["active_trial_loss"]
-active_pos = active_loss_data["pos"]
-active_target = active_loss_data["target"]
-active_postarget = active_loss_data["pos_target"]
+w = 9
+p = 7
 
-active_pos_sh = active_loss_data["pos_shuffle"]
-active_target_sh = active_loss_data["target_shuffle"]
+
+active_nce_data = data["active_trial_loss"]
+active_pos = active_nce_data["pos"]
+active_target = active_nce_data["target"]
+active_postarget = active_nce_data["pos_target"]
+
+active_pos_sh = active_nce_data["pos_shuffle"]
+active_target_sh = active_nce_data["target_shuffle"]
 
 c_id = np.linspace(50, 250, 7, dtype=int)
 
@@ -194,12 +202,58 @@ ax2.plot(
     c=plt.cm.tab10(3),
 )
 
+plt.xlim([0, 10000])
+plt.xlabel("Steps", fontsize=30)
+plt.xticks(np.linspace(0, 10000, 5), fontsize=20)
+plt.yticks(np.linspace(6.2, 5.6, 4), fontsize=20)
+plt.ylabel("Loss", fontsize=30)
+
+
+ax3 = fig.add_subplot(122)
+ax3.set_title("InfoMSE, active trials", fontsize=30)
+ax3.spines["top"].set_visible(False)
+ax3.spines["right"].set_visible(False)
+
+active_mse_data = data['active_mse_loss']
+active_pos = active_mse_data["pos"]
+active_target = active_mse_data["target"]
+active_postarget = active_mse_data["pos_target"]
+
+active_pos_sh = active_mse_data["pos_shuffled"]
+active_target_sh = active_mse_data["target_shuffled"]
+
+c_id = np.linspace(50, 250, 7, dtype=int)
+
+ax3.plot(
+    scipy.signal.savgol_filter(active_target, w, p), label="target", c=plt.cm.tab10(0)
+)
+
+ax3.plot(
+    scipy.signal.savgol_filter(active_target_sh, w, p),
+    label="target-shuffled",
+    c=plt.cm.tab10(0),
+    alpha=0.5,
+)
+
+ax3.plot(
+    scipy.signal.savgol_filter(active_pos, w, p), label="position", c=plt.cm.tab10(1)
+)
+ax3.plot(
+    scipy.signal.savgol_filter(active_pos_sh, w, p),
+    label="position-shuffled",
+    c=plt.cm.tab10(1),
+    alpha=0.5,
+)
+ax3.plot(
+    scipy.signal.savgol_filter(active_postarget, w, p),
+    label="position+target",
+    c=plt.cm.tab10(3),
+)
+
 
 plt.legend(fontsize="large", loc=[1, 0])
 plt.xlim([0, 10000])
 plt.xlabel("Steps", fontsize=30)
-plt.xticks(fontsize=20)
-plt.yticks(fontsize=20)
 plt.ylabel("Loss", fontsize=30)
 
 custom_lines = [
@@ -209,19 +263,17 @@ custom_lines = [
     Line2D([0], [0], lw=4, c=plt.cm.tab10(0), alpha=0.5),
     Line2D([0], [0], lw=4, c=plt.cm.tab10(1), alpha=0.5),
 ]
-ax2.legend(
+ax3.legend(
     custom_lines,
     ["target", "position", "position+target", "target shuffled", "position shuffled"],
-    loc=(1, 0),
+    loc=(1.1, 0),
     frameon=False,
-    fontsize="12",
+    fontsize="15",
 )
 
-plt.xlim([0, 10000])
-plt.xlabel("Steps", fontsize=30)
-plt.xticks(np.linspace(0, 10000, 5), fontsize=25)
-plt.yticks(np.linspace(6.2, 5.6, 4), fontsize=25)
-plt.ylabel("Loss", fontsize=30)
+plt.xticks(np.linspace(0,10000, 5),fontsize=20)
+plt.yticks([4.6,5.0,5.4,5.8,6.2],fontsize= 20)
+plt.xticks(np.linspace(0,10000, 5), fontsize = 20)
 # -
 
 
@@ -287,3 +339,57 @@ for i in range(2):
     ax.spines["top"].set_visible(False)
 
 plt.show()
+
+# +
+cebra_target = data['monkey_mse_viz']['cebra_target']['emission']
+target_label = data['monkey_mse_viz']['cebra_target']['label']
+cebra_pos = data['monkey_mse_viz']['cebra_pos']['emission']
+pos_label = data['monkey_mse_viz']['cebra_pos']['label']
+cebra_time = data['monkey_mse_viz']['cebra_time']['emission']
+
+
+fig = plt.figure(figsize=(12,5))
+plt.suptitle('CEBRA-behavior trained with target label using MSE loss', fontsize=20)
+ax=plt.subplot(121)
+ax.set_title('All trials embedding', fontsize=20, y=-0.1)
+x=ax.scatter(cebra_target[:,0], cebra_target[:,1],  c = target_label, cmap = plt.cm.hsv, s=0.05)
+ax.axis('off')
+ax=plt.subplot(122)
+ax.set_title('Post-averaged embs by direction', fontsize=20, y=-0.1)
+for i in range(8):
+    direction_trial = (target_label == i)
+    trial_avg = cebra_target[direction_trial, :].reshape(-1, 600, 2).mean(axis=0)
+    ax.scatter(trial_avg[:,0], trial_avg[:,1],  color=plt.cm.hsv(1 / 8 * i), s=3)
+ax.axis('off')
+
+fig = plt.figure(figsize=(12,5))
+plt.suptitle('CEBRA-behavior trained with position label using MSE loss', fontsize=20)
+ax=plt.subplot(121)
+ax.set_title('x', fontsize=20, y=0)
+x=ax.scatter(cebra_pos[:,0], cebra_pos[:,1],  c = pos_label[:,0], cmap = 'seismic', s=0.05,
+                vmin=-15,vmax=15)
+ax.axis('off')
+ax=plt.subplot(122)
+y=ax.scatter(cebra_pos[:,0], cebra_pos[:,1],  c = pos_label[:,1], cmap = 'seismic', s=0.05,
+            vmin=-15, vmax=15)
+ax.axis('off')
+ax.set_title('y', fontsize=20,y=0)
+yc = plt.colorbar(y, fraction=0.03, pad=0.05, ticks=np.linspace(-15, 15, 7))
+yc.ax.tick_params(labelsize=15)
+yc.ax.set_title("(cm)", fontsize=10)
+
+fig = plt.figure(figsize=(12,5))
+plt.suptitle('CEBRA-time traiend using MSE loss', fontsize=20)
+ax=plt.subplot(121)
+ax.set_title('x',  fontsize=20, y=-0.1)
+x=ax.scatter(cebra_time[:,0], cebra_time[:,1],  c = pos_label[:,0], cmap = 'seismic', s=0.05,
+                vmin=-15,vmax=15)
+ax.axis('off')
+ax=plt.subplot(122)
+y=ax.scatter(cebra_time[:,0], cebra_time[:,1],  c = pos_label[:,1], cmap = 'seismic', s=0.05,
+            vmin=-15, vmax=15)
+ax.axis('off')
+ax.set_title('y',  fontsize=20, y=-0.1)
+yc = plt.colorbar(y, fraction=0.03, pad=0.05, ticks=np.linspace(-15, 15, 7))
+yc.ax.tick_params(labelsize=15)
+yc.ax.set_title("(cm)", fontsize=10)
