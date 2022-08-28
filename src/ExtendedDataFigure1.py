94b0b3f558c1558b491a0991b994e992af8b75df
jinhl9 <haya.jinlee@gmail.com>
2022-08-28 21:56:06 +0200

    Migrate synthetic exp plotting function to the right file

--- src/ExtendedDataFigure1.py
+++ src/ExtendedDataFigure1.py
@@ -12,7 +12,7 @@
 #     name: python3
 # ---
 
-# # Extended Data Figure 1: Overview of datasets
+# # Figure S1: Overview of datasets
 
 # +
 import sys
@@ -64,9 +64,9 @@ plt.xticks(fontsize = 45)
 plt.yticks(np.linspace(0,1.6,3), fontsize = 45)
 plt.xticks(np.linspace(0, len(rat_behavior),5)*0.025, np.arange(0,45,10))
 cb_r_axes = fig.add_axes([0.92, 0.15, 0.02, 0.7])
-cb_l_axes = fig.add_axes([0.96, 0.15, 0.02, 0.7])
+cb_l_axes = fig.add_axes([0.96, 0.15, 0.02, 0.7]) 
 cb_r = plt.colorbar(ax_r, cax = cb_r_axes,boundaries=np.linspace(0,1.6, 200))
-cb_l = plt.colorbar(ax_l, cax = cb_l_axes, boundaries=np.linspace(0,1.6,200), ticks = np.linspace(0,1.6, 5))
+cb_l = plt.colorbar(ax_l, cax = cb_l_axes, boundaries=np.linspace(0,1.6,200), ticks = np.linspace(0,1.6, 5)) 
 cb_r.set_ticks([])
 
 cb_r.ax.set_xlabel('Right', fontsize = 15)
@@ -118,8 +118,8 @@ ax.spines['top'].set_visible(False)
 # ## Allen dataset
 
 # +
-neuropixel=data['mouse']['neural']['np']
-ca=data['mouse']['neural']['ca']
+neuropixel=data['mouse']['neural']['np'] 
+ca=data['mouse']['neural']['ca'] 
 fig=plt.figure(figsize=(15,10), dpi=300)
 plt.subplots_adjust(hspace=0.5)
 ax1=plt.subplot(2,1,1)
@@ -152,7 +152,6 @@ for n,i in enumerate(data['mouse']['behavior']):
     ax.imshow(i, cmap='gray')
     plt.axis('off')
 
-# ## Synthetic data experiments (panel b and c)
 
 # +
 def reindex(dic, list_name= ['poisson', 'gaussian','laplace', 'uniform','refractory_poisson']):
@@ -160,7 +159,7 @@ def reindex(dic, list_name= ['poisson', 'gaussian','laplace', 'uniform','refract
 
 def rename(df):
     return df.rename(columns = {'poisson': 'Poisson', 'gaussian': 'Gaussian', 'laplace': 'Laplace',
-                                              'uniform': 'uniform', 'refractory_poisson':
+                                              'uniform': 'uniform', 'refractory_poisson': 
                                               'refractory Poisson'})
 
 
@@ -187,195 +186,31 @@ legend_elements = [Line2D([0], [0], markersize=10,linestyle='none', marker = 'o'
 ax.legend(handles=legend_elements, loc = (1.0,-0.05), frameon=False, fontsize=15)
 sns.despine(left = False, right=True, bottom = False, top = True, trim = True, offset={'bottom':40, 'left':15})
 plt.savefig('distribution_reconstruction.png', transparent = True, bbox_inches='tight')
-# -
 
-z = data['noise_exp_viz']['z']
+# +
+pivae_embs=data['noise_exp_viz']['pivae']
+cebra_embs = data['noise_exp_viz']['cebra']
 label = data['noise_exp_viz']['label']
+z = data['noise_exp_viz']['z']
 def fitting(x,y):
     lin_model = sklearn.linear_model.LinearRegression()
     lin_model.fit(x,y)
     return lin_model.score(x,y), lin_model.predict(x)
-for i,dist in enumerate(['poisson', 'gaussian', 'laplace', 'uniform', 'refractory_poisson']) :
-    pivae_emission=data['noise_exp_viz']['pivae'][dist]
-    cebra_emission=data['noise_exp_viz']['cebra'][dist]
+emission_dict = {'pivae': {}, 'cebra':{}}
+    
+for i,dist in enumerate(['poisson', 'gaussian', 'laplace', 'uniform']) :
+    pivae_emission=pivae_embs[dist]
+    cebra_emission=cebra_embs[dist]
     cebra_score, fit_cebra = fitting(cebra_emission, z)
     pivae_score, fit_pivae = fitting(pivae_emission, z)
-    emission_dict['pivae'][dist] = pivae_emission
-    emission_dict['cebra'][dist] = cebra_emission
-    fig = plt.figure(figsize=(14,5))
+    fig = plt.figure(figsize=(12,5))
     plt.subplots_adjust(wspace = 0.3)
     ax=plt.subplot(121)
     ax.scatter(fit_cebra[:,0], fit_cebra[:,1], c = label, s=3, cmap = 'cool')
-    ax.set_title(f'CEBRA-{dist} \n$R^2$:{cebra_score:.2f}', fontsize=30)
+    ax.set_title(f'CEBRA-{dist}, $R^2$:{cebra_score:.2f}', fontsize=30)
     ax.axis('off')
     ax=plt.subplot(122)
     ax.scatter(fit_pivae[:,0], fit_pivae[:,1], c = label, s=3,cmap = 'cool')
-    ax.set_title(f'piVAE-{dist} \n$R^2$:{pivae_score:.2f}', fontsize=30)
+    ax.set_title(f'piVAE-{dist}, $R^2$:{pivae_score:.2f}', fontsize=30)
     ax.axis('off')
-
-
-# # original pi-VAE implementation vs. modified conv-pi-VAE
-
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-data = pd.read_hdf("../data/EDFigure1de.h5", key="data")
-
-# ## panel d
-
-for model in ["original_pivae", "conv_pivae"]:
-    embs = data[model]
-
-    sns.set_style("white")
-    fig = plt.figure(figsize=(10, 10))
-    plt.title(f"{model}- w/test time labels", fontsize=20, y=1.1)
-    plt.subplots_adjust(wspace=0.2, hspace=0.2)
-    plt.axis("off")
-    ind1, ind2 = 0, 1
-
-    for i in range(4):
-        ax = fig.add_subplot(2, 2, i + 1)
-        ax.set_title(f"Rat {i+1}", fontsize=1.5)
-        plt.axis("off")
-        emb = embs["w_label"]["embedding"][i]
-        label = embs["w_label"]["label"][i]
-        r_ind = label[:, 1] == 1
-        l_ind = label[:, 2] == 1
-        r = ax.scatter(
-            emb[r_ind, ind1], emb[r_ind, ind2], s=1, c=label[r_ind, 0], cmap="viridis"
-        )
-        l = ax.scatter(
-            emb[l_ind, ind1], emb[l_ind, ind2], s=1, c=label[l_ind, 0], cmap="cool"
-        )
-
-    fig = plt.figure(figsize=(10, 10))
-    plt.title(f"{model}- w/o test time labels", fontsize=20, y=1.1)
-    plt.subplots_adjust(wspace=0.2, hspace=0.2)
-    plt.axis("off")
-    for i in range(4):
-        ax = fig.add_subplot(2, 2, i + 1)
-        ax.set_title(f"Rat {i+1}", fontsize=15)
-        plt.axis("off")
-        emb = embs["wo_label"]["embedding"][i]
-        label = embs["wo_label"]["label"][i]
-        r_ind = label[:, 1] == 1
-        l_ind = label[:, 2] == 1
-        r = ax.scatter(
-            emb[r_ind, ind1], emb[r_ind, ind2], s=1, c=label[r_ind, 0], cmap="viridis"
-        )
-        l = ax.scatter(
-            emb[l_ind, ind1], emb[l_ind, ind2], s=1, c=label[l_ind, 0], cmap="cool"
-        )
-
-# ## panel e
-
-# +
-methods_name = ["w/test time labels", "without labels"]
-
-
-def prepare_heatmap(scores, n_item):
-    scores = scores.reshape(n_item, n_item - 1)
-    return np.array([np.insert(scores[i], i, None) for i in range(n_item)])
-
-
-fig, axs = plt.subplots(
-    ncols=3,
-    nrows=1,
-    figsize=(12, 5),
-    gridspec_kw={"width_ratios": [1, 1, 0.08]},
-    dpi=360,
-)
-fig.suptitle("original-pivae", y=1.1)
-subjects = ["Rat1", "Rat2", "Rat3", "Rat4"]
-scores = [
-    data["original_pivae"]["w_label"]["consistency"],
-    data["original_pivae"]["wo_label"]["consistency"],
-]
-
-sns.set(font_scale=2.0)
-
-for i, method in enumerate(methods_name):
-    score = prepare_heatmap(np.array(scores[i]), 4)
-    if i == 0:
-        hmap = sns.heatmap(
-            ax=axs[i],
-            data=score,
-            vmin=0.0,
-            vmax=100,
-            cmap=sns.color_palette("Greys", as_cmap=True),
-            annot=True,
-            xticklabels=subjects,
-            annot_kws={"fontsize": 16},
-            yticklabels=subjects,
-            cbar=False,
-        )
-    elif i == 1:
-        hmap = sns.heatmap(
-            ax=axs[i],
-            data=score,
-            vmin=0.0,
-            vmax=100,
-            cmap=sns.color_palette("Greys", as_cmap=True),
-            annot=True,
-            xticklabels=subjects,
-            annot_kws={"fontsize": 16},
-            yticklabels=subjects,
-            cbar_ax=axs[2],
-        )
-    hmap.set_title(method, fontsize=25)
-
-
-plt.subplots_adjust(wspace=0.3)
-axs[-1].set_xlabel("R2")
-
-fig, axs = plt.subplots(
-    ncols=3,
-    nrows=1,
-    figsize=(12, 5),
-    gridspec_kw={"width_ratios": [1, 1, 0.08]},
-    dpi=360,
-)
-fig.suptitle("Conv-pivae", y=1.1)
-subjects = ["Rat1", "Rat2", "Rat3", "Rat4"]
-scores = [
-    data["conv_pivae"]["w_label"]["consistency"],
-    data["conv_pivae"]["wo_label"]["consistency"],
-]
-
-sns.set(font_scale=2.0)
-
-for i, method in enumerate(methods_name):
-    score = prepare_heatmap(np.array(scores[i]), 4)
-    if i == 0:
-        hmap = sns.heatmap(
-            ax=axs[i],
-            data=score,
-            vmin=0.0,
-            vmax=100,
-            cmap=sns.color_palette("Greys", as_cmap=True),
-            annot=True,
-            xticklabels=subjects,
-            annot_kws={"fontsize": 16},
-            yticklabels=subjects,
-            cbar=False,
-        )
-    elif i == 1:
-        hmap = sns.heatmap(
-            ax=axs[i],
-            data=score,
-            vmin=0.0,
-            vmax=100,
-            cmap=sns.color_palette("Greys", as_cmap=True),
-            annot=True,
-            xticklabels=subjects,
-            annot_kws={"fontsize": 16},
-            yticklabels=subjects,
-            cbar_ax=axs[2],
-        )
-    hmap.set_title(method, fontsize=25)
-
-
-plt.subplots_adjust(wspace=0.3)
-axs[-1].set_xlabel("R2")
+    fig.savefig(f'emission_viz_{dist}.png', transparent=True, bbox_inches = 'tight')
