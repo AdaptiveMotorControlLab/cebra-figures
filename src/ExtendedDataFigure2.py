b714e01a857465c9de02593df8fd310179d10403
Mackenzie Mathis <mathis@rowland.harvard.edu>
2022-09-01 21:01:21 +0200

    WIP ED2

--- src/ExtendedDataFigure2.py
+++ src/ExtendedDataFigure2.py
@@ -6,22 +6,26 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.13.7
+#       jupytext_version: 1.14.1
 #   kernelspec:
-#     display_name: Python 3 (ipykernel)
+#     display_name: Python 3
 #     language: python
 #     name: python3
 # ---
 
 # # Extended Data Figure 2: Hyperparameter changes on visualization and consistency.
 
-# ## panel a
+# ### Temperature has the largest effect on visualization (vs. consistency) of the embedding
+#
+# - shown by a range from 0.1 to 3.21 (highest consistency for Rat 1), as can be appreciated in 3D (top) and post FastICA into a 2D embedding (middle). Bottom row shows the corresponding change on mean consistency. 
+#
+# - Note, tempature is both a learnable and easily modified parameter in CEBRA.
 
 # +
 # TODO: Temperature ablation plots
 # -
 
-# ## panel b
+# - Orange line denotes the median and black dots are individual runs (subject consistency: 10 runs with 3 comparisons per rat; run consistency: 10 runs, each compared to 9 remaining runs).
 
 # +
 # Consistency comparison across temperature values
