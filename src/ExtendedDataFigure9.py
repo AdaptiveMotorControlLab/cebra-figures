f43653091305becd16519b96c458a317f156582c
Mackenzie Mathis <mathis@rowland.harvard.edu>
2022-09-01 21:36:16 +0200

    ED9

--- src/ExtendedDataFigure9.py
+++ src/ExtendedDataFigure9.py
@@ -5,14 +5,16 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.13.7
+#       jupytext_version: 1.14.1
 #   kernelspec:
-#     display_name: Python 3 (ipykernel)
+#     display_name: Python [conda env:cebra_m1] *
 #     language: python
-#     name: python3
+#     name: conda-env-cebra_m1-py
 # ---
 
 # # Extended Data Figure 9: CEBRA produces consistent, highly decodable embeddings
+
+# #### import plot and data loading dependencies
 
 import numpy as np
 import pandas as pd
@@ -20,6 +22,10 @@ import matplotlib.pyplot as plt
 import sklearn.linear_model
 
 data = pd.read_hdf("../data/EDFigure9.h5", key="data")
+
+# ### Additional 4 sessions with the most neurons in the Allen visual dataset calcium recording shown for all algorithms we benchmarked
+#
+# - For CEBRA-Behavior and \cebra-Time, we used temperature 1, time offset 10, batch size 128 and 10k training steps. For UMAP, we used cosine metric and $n\_neighbors$ 15 and $min\_dist$ 0.1. For tSNE, we used cosine metric and $perplexity$ 30. For conv-pi-VAE, we trained with 600 epochs, batch size 200 and learning rate $5\times 10^{-4}$. All methods used 10 time bins input. CEBRA was trained with 3D latent and all other methods were obtained with 2D latent dimension.
 
 fig = plt.figure(figsize=(20, 24))
 emissions_list = [
