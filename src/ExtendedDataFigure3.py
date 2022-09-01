8107ede9ac1bdc798adce1b8d8075585ef055c12
Mackenzie Mathis <mathis@rowland.harvard.edu>
2022-09-01 21:02:40 +0200

    WIP ED3

--- src/ExtendedDataFigure3.py
+++ src/ExtendedDataFigure3.py
@@ -6,22 +6,16 @@
 #       extension: .py
 #       format_name: light
 #       format_version: '1.5'
-#       jupytext_version: 1.13.7
+#       jupytext_version: 1.14.1
 #   kernelspec:
-#     display_name: Python 3 (ipykernel)
+#     display_name: Python 3
 #     language: python
 #     name: python3
 # ---
 
 # # Extended Data Figure 3: CEBRA produces consistent, highly decodable embeddings
 
-# ## panel a
+# - Additional rat data shown for all algorithms we benchmarked (see Methods). CEBRA was trained with output latent 3D (the minimum) and all other methods were obtained with a 2D latent
 
 # +
 #
-# -
-
-# ## panel b
-
-# +
-# 
